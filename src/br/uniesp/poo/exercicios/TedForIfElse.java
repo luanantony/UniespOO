44a1c259fd21e1f826e2f7eb060b43963b453445
Luan Antony <luanantony29@gmail.com>
2022-04-22 23:15:24 -0300

    Commit Ted For if else

---
+++ src/br/uniesp/poo/exercicios/TedForIfElse.java
@@ -0,0 +1,53 @@
+package br.uniesp.poo.exercicios;
+
+import java.util.Scanner;
+
+public class TedForIfElse {
+    public static void main(String[] args) {
+        int sexo;
+        int qtdFem = 0;
+        int qtdMasc = 0;
+        double altura;
+        double somaMasc = 0;
+        double mediaMasc;
+        double maiorAlt = 0;
+        double menorAlt = 300;
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i<4; i++){
+            System.out.println("Entre com o sexo 0 para feminino ou 1 para masculino: ");
+            sexo = scan.nextInt();
+
+            System.out.println("Entre com a altura: ");
+            altura = scan.nextDouble();
+
+            if (sexo == 0){
+                System.out.println("Pessoa do sexo feminino com " + altura + " de altura.");
+                qtdFem++;
+            }else if (sexo == 1){
+                System.out.println("Pessoa do sexo masculino com " + altura + " de altura.");
+                qtdMasc++;
+                somaMasc = somaMasc + altura;
+            }else {
+                System.out.println("Entrada inválida. Tente novamente.");
+            }
+            if (altura < menorAlt){
+                menorAlt = altura;
+            } else if (altura > maiorAlt){
+                maiorAlt = altura;
+            }
+
+        }
+
+        mediaMasc = (somaMasc/qtdMasc);
+
+        System.out.println("A maior altura é: " + maiorAlt + "m");
+        System.out.println("A menor altura é: " + menorAlt + "m");
+        System.out.println("A média dos homens é: " + mediaMasc + "m");
+        System.out.println("A quantidade de mulheres é: " + qtdFem);
+
+        scan.close();
+    }
+
+}
